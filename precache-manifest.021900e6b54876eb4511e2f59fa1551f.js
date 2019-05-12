3a9d3a68214ea4c3f9edbddb179d3d8f749f56ad
omerco1 <31459523+omerco1@users.noreply.github.com>
2019-05-11 22:28:20 -0700

    Updates

---
+++ precache-manifest.021900e6b54876eb4511e2f59fa1551f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "96b71ac3ad36fb1ff84e23ed16f0e531",
+    "url": "/omercohen/static/media/avatar.96b71ac3.png"
+  },
+  {
+    "revision": "0d68683f5085aa05f173",
+    "url": "/omercohen/static/js/runtime~main.1de96f46.js"
+  },
+  {
+    "revision": "d84763553f8db62038b7",
+    "url": "/omercohen/static/js/main.b52f0900.chunk.js"
+  },
+  {
+    "revision": "f732bb185a8c2e63aec0",
+    "url": "/omercohen/static/js/2.6e112d0d.chunk.js"
+  },
+  {
+    "revision": "d84763553f8db62038b7",
+    "url": "/omercohen/static/css/main.81ace298.chunk.css"
+  },
+  {
+    "revision": "f732bb185a8c2e63aec0",
+    "url": "/omercohen/static/css/2.32eb76c0.chunk.css"
+  },
+  {
+    "revision": "dbde1caca493e676bfcdf75aa0d6274e",
+    "url": "/omercohen/index.html"
+  }
+];
